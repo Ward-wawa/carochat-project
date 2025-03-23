53cdf772c63ffbc34a80d6a0b792a52b3a515059
ward <wardistitan@gmail.com>
2025-03-23 23:45:17 +0300

    increased file limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve()
 
 dotenv.config();
-app.use(express.json());
+app.use(express.json({
+    limit: "50mb",
+    }
+));
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
